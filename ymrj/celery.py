93b51cf46a5433f603e722f6f70ade35e6b2590e
Rocky Meza <rockymeza@gmail.com>
2017-03-28 00:22:26 -0400

    Attempt to sentrify Celery

--- ymrj/celery.py
+++ ymrj/celery.py
@@ -1,8 +1,20 @@
 import os
+
 from celery import Celery
+import raven
+from raven.contrib.celery import register_signal, register_logger_signal
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ymrj.settings')
 
-app = Celery('ymrj')
+
+class InstrumentedCelery(Celery):
+    def on_configure(self):
+        client = raven.Client(os.environ.get('SENTRY_DSN'))
+        register_logger_signal(client)
+        register_signal(client)
+
+
+app = InstrumentedCelery('ymrj')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
