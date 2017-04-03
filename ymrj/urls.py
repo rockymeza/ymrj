e50590e48b7e5a3c7bf6b9c955b0a1e6c6134189
Rocky Meza <rockymeza@gmail.com>
2017-04-02 22:10:32 -0400

    Add welcome page

--- ymrj/urls.py
+++ ymrj/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='welcome.html')),
     url(r'^ingress/', include('ingress.urls')),
     url(r'^admin/', admin.site.urls),
 ]
